b11277ac260ae74cb3afbbbc97a189f1a0fae213
Nolan Trapp <officialstackemup@gmail.com>
2020-05-13 12:12:14 -0700

    1.2.0 UPDATE RELEASE

--- src/com/magicaldreams/hubmenu/Main.java
+++ src/com/magicaldreams/hubmenu/Main.java
@@ -1,5 +1,8 @@
 package com.magicaldreams.hubmenu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -23,19 +26,18 @@ public class Main extends JavaPlugin{
 		
 	}
 	
+	@SuppressWarnings("deprecation")
 	public static void applyHubMenuUI(Player player) {
-		Inventory HubMenuGUI = Bukkit.createInventory(null, 9, ChatColor.BLUE + "Hub Navigator");
 		
-		ItemStack Glass = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-		ItemMeta GlassMeta = Glass.getItemMeta();
-		GlassMeta.setDisplayName(" ");
-		Glass.setItemMeta(GlassMeta);
+		Inventory HubMenuGUI = Bukkit.createInventory(null, 9, ChatColor.BLUE + player.getName() + "'s Navigator");
 		
-		ItemStack skull = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
-        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-        skullMeta.setOwner(player.getName());
-        skullMeta.setDisplayName(ChatColor.YELLOW + player.getName());
-        skull.setItemMeta(skullMeta);
+
+		
+		ItemStack HUBskull = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
+        SkullMeta HUBskullMeta = (SkullMeta) HUBskull.getItemMeta();
+        HUBskullMeta.setOwner(player.getName());
+        HUBskullMeta.setDisplayName(ChatColor.YELLOW + player.getName());
+        HUBskull.setItemMeta(HUBskullMeta);
         
         ItemStack report = new ItemStack(Material.BARRIER);
         ItemMeta reportMeta = report.getItemMeta();
@@ -47,20 +49,85 @@ public class Main extends JavaPlugin{
         parksMeta.setDisplayName(ChatColor.YELLOW + "Parks");
         parks.setItemMeta(parksMeta);
 		
-	//  HubMenuGUI.setItem(0, Glass);
-	//	HubMenuGUI.setItem(1, Glass);
+
 		HubMenuGUI.setItem(2, report);
-	//	HubMenuGUI.setItem(3, Glass);
-		HubMenuGUI.setItem(4, skull);
-	//	HubMenuGUI.setItem(5, Glass);
+		HubMenuGUI.setItem(4, HUBskull);
 		HubMenuGUI.setItem(6, parks);
-	//	HubMenuGUI.setItem(7, Glass);
-	//	HubMenuGUI.setItem(8, Glass);
+
 
 		
 		player.openInventory(HubMenuGUI);
 
 	}
+	
+    public void applyHUBParksUI(Player player) {
+
+        //	ResultSet result = DriverManager.executeQuery("SELECT * FROM PlayerData WHERE BALANCE = 0;");		
+        //	List<String> bankruptPlayers = new ArrayList<String>();
+        //while (result.next()) {
+        //    String name = result.getString("PLAYERNAME");
+        //    bankruptPlayers.add(name);
+        //	}
+        List<String> parksLore = new ArrayList<>();
+        //	parksLore.add(ChatColor.GRAY + "Ride is: " + );
+
+
+        Inventory ParksGUI = Bukkit.createInventory(null, 27, ChatColor.BLUE + "Parks");
+
+        ItemStack back = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+        ItemMeta backMeta = back.getItemMeta();
+        backMeta.setDisplayName(ChatColor.GREEN + "<-- Back");
+        back.setItemMeta(backMeta);
+
+        ItemStack parks = new ItemStack(Material.NETHER_STAR);
+        ItemMeta parksMeta = parks.getItemMeta();
+        parksMeta.setDisplayName(ChatColor.YELLOW + "Parks");
+        parks.setItemMeta(parksMeta);
+
+        ItemStack fish = new ItemStack(Material.TROPICAL_FISH);
+        ItemMeta fishMeta = fish.getItemMeta();
+        fishMeta.setDisplayName(ChatColor.YELLOW + "Join SWSA");
+        fish.setItemMeta(fishMeta);
+
+        ItemStack key = new ItemStack(Material.DIAMOND_HOE);
+        ItemMeta keyMeta = key.getItemMeta();
+        keyMeta.setDisplayName(ChatColor.AQUA + "Join DLR");
+        key.setItemMeta(keyMeta);
+
+        ItemStack DAXE = new ItemStack(Material.DIAMOND_PICKAXE);
+        ItemMeta DAXEMeta = DAXE.getItemMeta();
+        DAXEMeta.setDisplayName(ChatColor.AQUA + "Join Creative");
+        DAXE.setItemMeta(DAXEMeta);
+
+        ItemStack Mickey = new ItemStack(Material.POTATO);
+        ItemMeta MickeyMeta = Mickey.getItemMeta();
+        MickeyMeta.setDisplayName(ChatColor.GOLD + "Join TDL");
+        Mickey.setItemMeta(MickeyMeta);
+
+        ItemStack BOOK = new ItemStack(Material.ENCHANTED_BOOK);
+        ItemMeta BOOKMeta = BOOK.getItemMeta();
+        BOOKMeta.setDisplayName(ChatColor.BOLD + "Join HUB");
+        BOOK.setItemMeta(BOOKMeta);
+
+        ItemStack GAXE = new ItemStack(Material.GOLDEN_PICKAXE);
+        ItemMeta GAXEMeta = GAXE.getItemMeta();
+        GAXEMeta.setDisplayName(ChatColor.GOLD + "Join Custom");
+        GAXE.setItemMeta(GAXEMeta);
+
+
+        ParksGUI.setItem(4, parks);
+        ParksGUI.setItem(12, fish);
+        ParksGUI.setItem(13, key);
+        ParksGUI.setItem(14, DAXE);
+        ParksGUI.setItem(21, Mickey);
+        ParksGUI.setItem(22, BOOK);
+        ParksGUI.setItem(23, GAXE);
+        ParksGUI.setItem(0, back);
+
+        player.openInventory(ParksGUI);
+
+
+    }
 	
 	@Override
 	public void onDisable() {
--- src/com/magicaldreams/hubmenu/OpenHub.java
+++ src/com/magicaldreams/hubmenu/OpenHub.java
@@ -1,5 +1,10 @@
 package com.magicaldreams.hubmenu;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.bukkit.*;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
@@ -8,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
@@ -15,6 +21,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import com.magicaldreams.hubmenu.Main;
 
+@SuppressWarnings("unused")
 public class OpenHub implements Listener {
 	
 	 ItemStack JoinMBHub;
@@ -38,14 +45,113 @@ public class OpenHub implements Listener {
 
 	        JoinMBHub.setItemMeta(JoinMBHubMeta);
 
-	        player.getInventory().setItem(4, JoinMBHub);
+	        player.getInventory().setItem(0, JoinMBHub);
 
 
 	    }
 	    
-	    
-	    @SuppressWarnings({ "deprecation", "unlikely-arg-type" })
-	    @EventHandler
+
+
+
+	        @SuppressWarnings("deprecation")
+		    @EventHandler
+		    public void onClick(InventoryClickEvent e) {
+
+		        Player player = (Player) e.getWhoClicked();
+
+		        if(e.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + player.getName() + "'s Navigator")) {
+
+		            if(e.getCurrentItem() != null) {
+		                e.setCancelled(true);
+		                switch (e.getCurrentItem().getType()) {
+	                case BARRIER:
+	                	
+	                	 player.sendMessage(ChatColor.YELLOW
+	                                + "Click this link to Report/Issue: https://docs.google.com/forms/d/e/1FAIpQLSf-7fXP9KbaQ6FcpzPaqBA7eeqtBIr1mc0cfxFtQmHMBNp_dg/viewform");
+	                        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "-------------------- " + player.getName() + " has to report something --------------------");
+	                        player.sendMessage(ChatColor.RED + "The staff has been notified of your issue!(Shows up in console log)");
+	                        player.closeInventory();
+
+	                        break;
+                    case NETHER_STAR:
+                        main.applyHUBParksUI((Player) player);
+	                        
+                    break;
+                default:
+                    return;
+
+	                }
+	            }
+	            
+
+	        }
+	    }
+	        
+	        
+	        @EventHandler
+	        public void ParksonClick(InventoryClickEvent e) {
+
+
+
+	            Player player = (Player) e.getWhoClicked();
+
+
+	            if( e.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + "Parks")) {
+
+	                if(e.getCurrentItem() != null) {
+	                    e.setCancelled(true);
+	                    switch (e.getCurrentItem().getType()) {
+	                        case GREEN_STAINED_GLASS_PANE:
+	                            Main.applyHubMenuUI((Player) player);
+
+	                            break;
+	                        case TROPICAL_FISH:
+	                            player.performCommand("join swsa");
+	                            player.sendMessage(ChatColor.YELLOW + "Attempting to send you to: SWSA");
+	                            player.closeInventory();
+
+	                            break;
+	                        case DIAMOND_HOE:
+	                            player.performCommand("join dlr");
+	                            player.sendMessage(ChatColor.YELLOW + "Attempting to send you to: DLR");
+	                            player.closeInventory();
+
+	                            break;
+	                        case DIAMOND_PICKAXE:
+	                            player.performCommand("join creative");
+	                            player.sendMessage(ChatColor.YELLOW + "Attempting to send you to: Creative");
+	                            player.closeInventory();
+
+	                            break;
+	                        case POTATO:
+	                            player.performCommand("join tdr");
+	                            player.sendMessage(ChatColor.YELLOW + "Attempting to send you to: TDR");
+	                            player.closeInventory();
+
+	                            break;
+	                        case ENCHANTED_BOOK:
+	                            player.performCommand("join hub");
+	                            player.sendMessage(ChatColor.YELLOW + "Attempting to send you to: HUB");
+	                            player.closeInventory();
+
+	                            break;
+	                        case GOLDEN_PICKAXE:
+	                            player.performCommand("join custom");
+	                            player.sendMessage(ChatColor.YELLOW + "Attempting to send you to: Custom");
+	                            player.closeInventory();
+
+	                            break;
+	                        default:
+	                            return;
+	                    }
+	                }
+
+	            }
+
+	        }
+	     
+	    @SuppressWarnings("deprecation")
+		@EventHandler
 	    public static void PlayerMB(PlayerInteractEvent e){
 	        Action action = e.getAction();
 	        Player player = (Player) e.getPlayer();
@@ -61,5 +167,8 @@ public class OpenHub implements Listener {
 
 	        }
 	    }
+	    
+	    
+	   
 
 }
